8933271954d2f2fe74336c420a24e5ee1d4af0d2
Vincent Bernat <vincent@bernat.ch>
2024-08-18 21:54:33 +0200

    cloudfront: update to use a cache policy
    Also disable compression, just forward what the server provides,
    including Brotli compression.

--- luffy/cloudfront.ts
+++ luffy/cloudfront.ts
@@ -1,72 +1,101 @@
 import { Construct } from "constructs";
 import { AwsProvider } from "../.gen/providers/aws/provider";
 import { CloudfrontDistribution } from "../.gen/providers/aws/cloudfront-distribution";
+import { CloudfrontCachePolicy } from "../.gen/providers/aws/cloudfront-cache-policy";
 
 /* Cookie-less cloudfront distribution. */
 class MediaCloudfrontDistribution extends Construct {
-  constructor(protected scope: Construct, protected domain: string, protected provider: AwsProvider) {
+  constructor(
+    protected scope: Construct,
+    protected domain: string,
+    protected cachePolicy: CloudfrontCachePolicy,
+    protected provider: AwsProvider
+  ) {
     super(scope, domain);
     const originId = "MyOrigin";
-    new CloudfrontDistribution(
-      this,
-      domain,
-      {
-        provider,
-        enabled: true,
-        isIpv6Enabled: true,
-        httpVersion: "http2and3",
-        priceClass: "PriceClass_All",
-        viewerCertificate: {
-          cloudfrontDefaultCertificate: true,
-        },
-        restrictions: {
-          geoRestriction: {
-            restrictionType: "none",
-          },
-        },
-
-        origin: [
-          {
-            originId,
-            originPath: "",
-            domainName: domain,
-            customOriginConfig: {
-              httpPort: 80,
-              httpsPort: 443,
-              originKeepaliveTimeout: 5,
-              originProtocolPolicy: "https-only",
-              originReadTimeout: 30,
-              originSslProtocols: ["TLSv1.2"],
-            },
-          },
-        ],
-        defaultCacheBehavior: {
-          minTtl: 0,
-          defaultTtl: 86400,
-          maxTtl: 31536000,
-          allowedMethods: ["GET", "HEAD"],
-          cachedMethods: ["GET", "HEAD"],
-          targetOriginId: originId,
-          viewerProtocolPolicy: "allow-all",
-          compress: true,
-          forwardedValues: {
-            cookies: {
-              forward: "none",
-              whitelistedNames: [],
-            },
-            headers: ["Accept"],
-            queryString: false,
-          },
+    new CloudfrontDistribution(this, domain, {
+      provider,
+      enabled: true,
+      isIpv6Enabled: true,
+      httpVersion: "http2and3",
+      priceClass: "PriceClass_All",
+      viewerCertificate: {
+        cloudfrontDefaultCertificate: true,
+      },
+      restrictions: {
+        geoRestriction: {
+          restrictionType: "none",
         },
       },
-    );
+      origin: [
+        {
+          originId,
+          originPath: "",
+          domainName: domain,
+          customOriginConfig: {
+            httpPort: 80,
+            httpsPort: 443,
+            originKeepaliveTimeout: 5,
+            originProtocolPolicy: "https-only",
+            originReadTimeout: 30,
+            originSslProtocols: ["TLSv1.2"],
+          },
+        },
+      ],
+      defaultCacheBehavior: {
+        allowedMethods: ["GET", "HEAD"],
+        cachedMethods: ["GET", "HEAD"],
+        targetOriginId: originId,
+        viewerProtocolPolicy: "allow-all",
+        compress: false,
+        cachePolicyId: cachePolicy.id,
+      },
+    });
   }
 }
 
 export class Resources extends Construct {
   constructor(protected scope: Construct, protected provider: AwsProvider) {
     super(scope, "luffy-cloudfront");
-    new MediaCloudfrontDistribution(this, "media.bernat.ch", provider);
-    new MediaCloudfrontDistribution(this, "media.une-oasis-une-ecole.fr", provider);
+    const cachePolicy = new CloudfrontCachePolicy(
+      this,
+      "CachingOptimized-Accept",
+      {
+        name: "CachingOptimized-Accept",
+        comment:
+          "Policy with caching enabled. Gzip and Brotli compression. Forwards Accept header.",
+        defaultTtl: 86400,
+        maxTtl: 31536000,
+        minTtl: 0,
+        parametersInCacheKeyAndForwardedToOrigin: {
+          enableAcceptEncodingGzip: true,
+          enableAcceptEncodingBrotli: true,
+          cookiesConfig: {
+            cookieBehavior: "none",
+          },
+          headersConfig: {
+            headerBehavior: "whitelist",
+            headers: {
+              items: ["Accept"],
+            },
+          },
+          queryStringsConfig: {
+            queryStringBehavior: "none",
+          },
+        },
+      }
+    );
+    new MediaCloudfrontDistribution(
+      this,
+      "media.bernat.ch",
+      cachePolicy,
+      provider
+    );
+    new MediaCloudfrontDistribution(
+      this,
+      "media.une-oasis-une-ecole.fr",
+      cachePolicy,
+      provider
+    );
   }
 }
